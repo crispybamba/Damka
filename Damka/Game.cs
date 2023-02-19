a4e0f62511160d0ffde1643f923a28c9366306d2
student <student@A26-17>
2023-02-19 09:45:19 +0200

    s

--- Damka/Game.cs
+++ Damka/Game.cs
@@ -8,5 +8,12 @@ namespace Damka
 {
     internal class Game
     {
+        private Player player1;
+        private Player player2;
+        public Game()
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
     }
 }
